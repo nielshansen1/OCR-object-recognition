3f7f25788475051466238ff400f7fc332a80e701
nielshansen1 <114566358+nielshansen1@users.noreply.github.com>
2023-03-30 14:46:32 +0200

    Add files via upload

--- classifier.py
+++ classifier.py
@@ -1,11 +1,14 @@
 #! /usr/bin/python3
 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing import image as IMAGE
 import os
 import time
-import picamera
 import cv2 as cv
 import numpy as np
 import pytesseract as tess
+from picamera2 import Picamera2, Preview
 from guizero import App, Picture, Text, PushButton, Window, TextBox
 from PIL import Image
 import tensorflow as tf
@@ -14,40 +17,53 @@ import tensorflow as tf
 gui = App(title="Classifier System", height=2000, width=2000, layout="grid")
 
 # Create the camera with custom settings
-camera = picamera.PiCamera()
-camera.resolution = (2028, 1520)
-camera.shutter_speed = 2500
+camera = Picamera2()
+camera.configure(camera.create_preview_configuration())
+camera.set_controls({"ExposureTime": 2500})
 
-# Start the preview
-camera.start_preview(fullscreen=False, window = (0,0,600,1600))
-
-# functies voor maken v foto 
+# Start the camera
+camera.start()
+	
+# take picture & apply ocr
 def take_picture():
 	# generate filename for the created picture
-	filename = '/home/raspberry/camera/train/'+ time.strftime("%Y%m%d-%H%M%S")+".jpg"
-	filename_resized = time.strftime("%Y%m%d-%H%M%S")+"_resized.jpg"
+	filename = '/home/pi/camera/train/'+ time.strftime("%Y%m%d-%H%M%S")+".png"
+	filename_resized = '/home/pi/camera/train/'+ time.strftime("%Y%m%d-%H%M%S")+"_resized.png"
 	# create picture with generated filename
-	camera.capture(filename)
+	camera.capture_file(filename)
 	# half the size of the picture 
 	with Image.open(filename) as img:
 		resized_img = img.resize((img.width//3, img.height//3))
 		resized_img.save(filename_resized)
 	# update the picture widget with the new image
-	picture.value=filename_resized
-	
+	picture.value=filename	
 	# OCR on big picture!
 	image = cv.imread(filename)
 	tesseract_to_string(image)
+	usemodel(filename)
 
 def picture_to_DS():
 	dirname = textbox.value
-	path = os.path.join("/home/raspberry/camera/train/", dirname)
+	path = os.path.join("/home/pi/camera/train/", dirname)
 	if not os.path.exists(path):
 		os.mkdir(path,mode=0o777)
 	filename = path + "/" + time.strftime("%Y%m%d-%H%M%S")+".jpg"
 	print(filename)
-	camera.capture(filename)
-	print('file saved')
+	camera.capture_file(filename)
+	
+def usemodel(img_filename):
+	testimg = IMAGE.load_img(img_filename, target_size=(200,200))
+	x = IMAGE.img_to_array(testimg)
+	x = np.expand_dims(x, axis=0)
+	val = model.predict(x)
+	print(val)
+'''	if val > 0.5:
+		print("doos")
+		class_text.value = "doos"+str(val)
+	else:
+		print("boek")
+		class_text.value = "boek"+str(val)
+'''	
 	
 def remove_preview():
 	camera.stop_preview()
@@ -94,8 +110,8 @@ def sharpen(image):
 # OCR (with preprocessing)
 def tesseract_to_string(image):
 	# Generate filename for ocr image
-	filename_OCR = time.strftime("%Y%m%d-%H%M%S")+"_OCR.jpg"
-	filename_OCR_resized = time.strftime("%Y%m%d-%H%M%S")+"_OCR_resized.jpg"
+	filename_OCR = time.strftime("%Y%m%d-%H%M%S")+"_OCR.png"
+	filename_OCR_resized = time.strftime("%Y%m%d-%H%M%S")+"_OCR_resized.png"
 	# increase brightness
 	bright = brightness_increase(image, 1) 
 	# filter out noise pixels  
@@ -119,7 +135,7 @@ def tesseract_to_string(image):
 	with Image.open(filename_OCR) as img:
 		resized_ocr_img = img.resize((img.width//3, img.height//3))
 		resized_ocr_img.save(filename_OCR_resized)
-	picture_ocr.value=filename_OCR_resized
+		picture_ocr.value=filename_OCR
 	
 # Starts a learning window where new product can be added to dataset
 def learning():
@@ -132,9 +148,44 @@ def learning():
 
 # Starts training the updated dataset	
 def train():
-	#data = DataLoader.from_folder("/home/raspberry/camera/train")
-	#model=image_classifier.create(train_data)
-	#model.export(export_dir="/home/raspberry/camera")
+	global model
+	# waardes van 0-1 ipv 0-255
+	train = ImageDataGenerator(rescale=1/255)
+	validation = ImageDataGenerator(rescale=1/255)
+
+	train_dataset= train.flow_from_directory("/home/pi/camera/train/",
+											 target_size=(200, 200),
+											 batch_size = 1,
+											 class_mode = "binary") #vanwege 2 classes: voor & achterkant
+
+	validation_dataset= validation.flow_from_directory("/home/pi/camera/validation/",
+														target_size=(200, 200),
+														batch_size = 3,
+														class_mode = "binary") #vanwege 2 classes: voor & achterkant
+														
+	model = tf.keras.models.Sequential([tf.keras.layers.Conv2D(16,(3,3),activation='relu', input_shape=(200,200,3)),
+										tf.keras.layers.MaxPool2D(2,2),
+										#
+										tf.keras.layers.Conv2D(32,(3,3),activation='relu', input_shape=(200,200,3)),
+										tf.keras.layers.MaxPool2D(2,2),
+										#
+										tf.keras.layers.Conv2D(64,(3,3),activation='relu', input_shape=(200,200,3)),
+										tf.keras.layers.MaxPool2D(2,2),
+										##
+										tf.keras.layers.Flatten(),
+										##
+										tf.keras.layers.Dense(512,activation="relu"),
+										##
+										tf.keras.layers.Dense(1,activation="sigmoid")
+										])
+										
+	model.compile(loss='binary_crossentropy',
+				  optimizer = RMSprop(learning_rate=0.001),
+				  metrics = ['accuracy'])
+
+	print(validation_dataset.class_indices)
+
+	model_fit= model.fit(train_dataset, steps_per_epoch = 10, epochs=40, validation_data=validation_dataset)
 	return 0 
 	
 # Create Picture widget
@@ -142,10 +193,13 @@ picture = Picture(gui, grid=[2,0])
 picture_ocr = Picture(gui, grid=[3,0])
 
 # Create button to make the pictures
-pic_button = PushButton(gui, grid=[0,0],text="take image",command = take_picture)
+pic_button = PushButton(gui, grid=[0,0], text="take image", command = take_picture)
 
-# Create Text widget
+# Create OCRText widget
 ocrtext = Text(gui, grid=[1,0])
+
+# Create Classifier Text widget
+class_text = Text(gui, grid=[1,1])
 
 # Create learning button for system to create new product
 learn_button = PushButton(gui, grid=[0,2], text="add new product to database", command = learning)
@@ -158,6 +212,4 @@ cam_button = PushButton(gui, grid=[0,1], text="remove preview", command = remove
 
 # Run the GUI
 gui.display()
-
-
 
