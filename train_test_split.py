4f662463ae16da9edcdb1fd729fac9d43f074767
nielshansen1 <114566358+nielshansen1@users.noreply.github.com>
2023-04-11 15:27:02 +0200

    Update train_test_split.py
    volledige code is aangepast

--- train_test_split.py
+++ train_test_split.py
@@ -2,59 +2,47 @@ import os
 import random
 import shutil
 
-def split_train_test(source_directory, target_directory, train_percentage=0.8):
-    """
-    Function to perform train/test split of images of packaging in a source directory.
+train_dir = "train"
+validation_dir = "validation"
 
-    Args:
-        source_directory (str): Path to the source directory.
-        target_directory (str): Path to the target directory where the train/test splits will be created.
-        train_percentage (float, optional): Percentage for the train/test split (default is 0.8 for 80% train and 20% test).
-    """
-    # Create train and test directories
-    train_directory = os.path.join(target_directory, "train")
-    test_directory = os.path.join(target_directory, "test")
-    os.makedirs(train_directory, exist_ok=True)
-    os.makedirs(test_directory, exist_ok=True)
+# Lijst van koeksoorten
+koeksoorten = os.listdir(train_dir)
 
-    # Collect all images in the source directory and subdirectories
-    images = []
-    for root, dirs, files in os.walk(source_directory):
-        for file in files:
-            # Only include files with a certain extension (e.g., .jpg)
-            if file.endswith(".jpg"):
-                source_path = os.path.join(root, file)
-                images.append(source_path)
-
-    # Loop through subdirectories in the source directory
-    for directory in dirs:
-        # Create corresponding train and test subdirectories
-        train_subdirectory = os.path.join(train_directory, directory)
-        test_subdirectory = os.path.join(test_directory, directory)
-        os.makedirs(train_subdirectory, exist_ok=True)
-        os.makedirs(test_subdirectory, exist_ok=True)
-
-        # Filter images that belong to the current subdirectory
-        subdirectory_images = [image for image in images if directory in image]
-
-        # Calculate the number of images for the train/test split
-        total_images = len(subdirectory_images)
-        train_images = int(total_images * train_percentage)
-        test_images = total_images - train_images
-
-        # Shuffle the images
-        random.shuffle(subdirectory_images)
-
-        # Copy train images to the train subdirectory
-        for image in subdirectory_images[:train_images]:
-            filename = os.path.basename(image)
-            target_path = os.path.join(train_subdirectory, filename)
-            shutil.copy2(image, target_path)
-
-        # Copy test images to the test subdirectory
-        for image in subdirectory_images[train_images:]:
-            filename = os.path.basename(image)
-            target_path = os.path.join(test_subdirectory, filename)
-            shutil.copy2(image, target_path)
-
-    print("Train/Test split completed!")
+# Loop door de koeksoorten en voer de train/test split uit
+for koeksoort in koeksoorten:
+    koeksoort_dir = os.path.join(train_dir, koeksoort)
+    validation_koeksoort_dir = os.path.join(validation_dir, koeksoort)
+    
+    # Controleer of de directory al bestaat in de validation directory
+    if not os.path.exists(validation_koeksoort_dir):
+        os.makedirs(validation_koeksoort_dir)
+    
+    # Lijst van bestanden in de koeksoort directory
+    bestanden = os.listdir(koeksoort_dir)
+    
+    # Lijst van bestanden die al in de validation directory staan
+    validation_bestanden = os.listdir(validation_koeksoort_dir)
+    
+    # Bereken het aantal bestanden dat verplaatst moet worden voor de test set
+    n_validation = int(0.2 * len(bestanden))
+    
+    # Controleer of er voldoende bestanden zijn voor de test set
+    if n_validation >= len(bestanden):
+        raise ValueError("Niet voldoende bestanden in de directory {} voor een 80/20 split.".format(koeksoort_dir))
+    
+    # Bereken het aantal bestanden dat nog verplaatst kan worden
+    n_available = len(bestanden) - len(validation_bestanden)
+    
+    # Controleer of er voldoende beschikbare bestanden zijn voor de test set
+    if n_validation > n_available:
+        raise ValueError("Niet voldoende beschikbare bestanden in de directory {} voor een 80/20 split.".format(koeksoort_dir))
+    
+    # Kies willekeurig bestanden voor de test set
+    validation_bestanden += random.sample([bestand for bestand in bestanden if bestand not in validation_bestanden], n_validation)
+    
+    # Verplaats de gekozen bestanden naar de validation directory
+    for bestand in validation_bestanden:
+        bestand_src = os.path.join(koeksoort_dir, bestand)
+        bestand_dst = os.path.join(validation_koeksoort_dir, bestand)
+        shutil.move(bestand_src, bestand_dst)
+        print("Bestand {} verplaatst naar {}".format(bestand, validation_koeksoort_dir))
